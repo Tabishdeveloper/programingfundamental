ad71f8d885c7caa701848b09e5b7166190aa5c21
Tabishdeveloper <faziff1389@gmail.com>
2023-10-31 17:47:43 +0500

    setw

---
+++ Manipulator Setw/index.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+int main(){
+
+    int i,n;
+cout<<"enter number\n";
+cin>>n;
+        for( i=1;i<=10;++i){        
+            for(int j=1;j<=n;++j){
+                 
+               cout<<setw(5)<<j<<" * "<<i <<"="<<i*j<<"\t"; 
+            
+             
+                   
+                               
+                    }  
+                  
+                   cout<<"\n";
+                  
+                          
+            }   
+    return 0 ;
+}
