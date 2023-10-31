eec3689739354e82d9d7ccaf5af9f07e0c7f6985
Tabishdeveloper <faziff1389@gmail.com>
2023-10-31 10:45:26 +0500

    star pattern

---
+++ Star Pattern/index.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    // Star Pattern
+
+
+     int  i, j, n;
+cout << "Enter number of rows:  ";
+cin >> n;
+for(i = 1; i <= n; i++)
+{
+for(j = 1; j <= i; j++)
+{
+cout << "* ";
+}
+
+cout << "\n";
+}          
+    return 0 ;
+}
